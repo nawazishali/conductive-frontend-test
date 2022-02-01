318fb8271e60415c98b8c8efeaadf38d80ac2267
Nawazish <n.ali@metru.io>
2022-02-02 02:53:34 +0500

    Configure base D3.js sankey graph with dummy data.

--- src/components/Sankey.vue
+++ src/components/Sankey.vue
@@ -1,58 +1,121 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <div ref="sankey"></div>
 </template>
 
 <script>
+// Code referenced from https://bl.ocks.org/d3noob/31665aced416f27abca4fa46f5f4b568
+import * as d3 from "d3";
+import csvtojson from "csvtojson";
+import csv from "@/data.js"
+import { sankey as Sankey, sankeyLinkHorizontal } from "d3-sankey";
+
 export default {
-  name: 'HelloWorld',
+  name: "Sankey",
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+  async mounted() {
+    
+    // set the dimensions and margins of the graph
+    let margin = { top: 10, right: 10, bottom: 10, left: 10 },
+      width = 900 - margin.left - margin.right,
+      height = 300 - margin.top - margin.bottom;
+
+    // format variables
+    let formatNumber = d3.format(",.0f"), // zero decimal places
+      format = (d) => formatNumber(d),
+      color = d3.scaleOrdinal(d3.schemeCategory10);
+    // append the svg object to the body of this component
+    let svg = d3
+      .select(this.$refs.sankey)
+      .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    let sankey = Sankey().nodeWidth(20).nodePadding(40).size([width, height]);
+
+    // load the data
+    let data = await d3.json(
+      "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/data_sankey.json"
+    );
+
+    let graph = sankey(data);
+
+    // add in the links
+    let link = svg
+      .append("g")
+      .selectAll(".link")
+      .data(graph.links)
+      .enter()
+      .append("path")
+      .attr("class", "link")
+      .attr("d", sankeyLinkHorizontal())
+      .attr("stroke-width", (d) => d.width);
+
+    // add the link titles
+    link
+      .append("title")
+      .text(
+        (d) => d.source.name + " → " + d.target.name + "\n" + format(d.value)
+      );
+
+    // add in the nodes
+    let node = svg
+      .append("g")
+      .selectAll(".node")
+      .data(graph.nodes)
+      .enter()
+      .append("g")
+      .attr("class", "node");
+
+    // add the rectangles for the nodes
+    node
+      .append("rect")
+      .attr("x", (d) => d.x0)
+      .attr("y", (d) => d.y0)
+      .attr("height", (d) => d.y1 - d.y0)
+      .attr("width", sankey.nodeWidth())
+      .style("fill", (d) => (d.color = color(d.name.replace(/ .*/, ""))))
+      .style("stroke", (d) => d3.rgb(d.color).darker(2))
+      .append("title")
+      .text((d) => d.name + "\n" + format(d.value));
+
+    // add in the title for the nodes
+    node
+      .append("text")
+      .attr("x", (d) => d.x0 - 6)
+      .attr("y", (d) => (d.y1 + d.y0) / 2)
+      .attr("dy", "0.35em")
+      .attr("text-anchor", "end")
+      .text((d) => d.name)
+      .filter((d) => d.x0 < width / 2)
+      .attr("x", (d) => d.x1 + 6)
+      .attr("text-anchor", "start");
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style>
+.node rect {
+  fill-opacity: 0.9;
+  shape-rendering: crispEdges;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.node text {
+  pointer-events: none;
+  text-shadow: 0 1px 0 #fff;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.link {
+  fill: none;
+  stroke: #000;
+  stroke-opacity: 0.2;
 }
-a {
-  color: #42b983;
+
+.link:hover {
+  stroke-opacity: 0.5;
 }
 </style>
